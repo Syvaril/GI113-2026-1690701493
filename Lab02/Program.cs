e02f032698e21cdaa1e2430df5b24f540a19a8a8
Syvaril <104825928+Syvaril@users.noreply.github.com>
2026-08-24 17:03:42 +0700

    Add Info

---
+++ Lab02/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * Student ID : 1690701493
+ * Name       : Thanonchai Kruttha
+ * Section    : 129B
+ * No.        : NA
+ * Course     : GI113 Computer Programming (GI)
+ */
+
+namespace Lab02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
